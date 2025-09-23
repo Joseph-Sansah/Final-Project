6aab277f743e6e103c66bbdd09fff0b401a8765e
Sansah Joseph <sansahjosrph127@gmail.com>
2025-09-23 01:26:35 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
-import os
 from app import create_app, socketio
+import os
 
 app = create_app()
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
     socketio.run(
         app,
-        debug=False,
         host="0.0.0.0",
-        port=port
+        port=port,
+        allow_unsafe_werkzeug=True
     )
